212f0cbe3e967d1dfa65764dfd4de50f87c1aa52
sozdatel31 <pavlik.gerasim@gmail.com>
2021-06-20 13:39:31 +0300

    first page +

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import './App.css';
+import style from './App.module.css';
 import Header from "./Header/Header";
 import Main from "./Main/Main";
 
 function App() {
     return (
-        <div className="App">
+        <div className={style.App}>
             <Header/>
             <Main/>
         </div>
--- src/Main/Main.jsx
+++ src/Main/Main.jsx
@@ -5,14 +5,17 @@ function Main() {
     return (
         <div className={style.header}>
             <div className={style.container}>
-                <div className={style.avatar}>
-                <div className={style.image}><img src="https://prikolist.club/wp-content/uploads/2019/06/0-245.jpg" alt="avatar"/></div>
+                <div className={style.container2}>
+                    <div className={style.content}>
+                        <div className={style.avatar}>
+                            <div className={style.image}></div>
+                        </div>
+                        <div className={style.details}>
+                            <h3 className={style.name}>PAVEL HERASIMCHUK </h3>
+                            <p className={style.job}>frontend developer from Minsk, Belarus</p>
+                        </div>
+                    </div>
                 </div>
-                <div className={style.details}>
-                    <h3 className={style.name}>PAVEL HERASIMCHUK </h3>
-                    <p className={style.job}>frontend developer from Minsk</p>
-                </div>
-
             </div>
         </div>
     );
